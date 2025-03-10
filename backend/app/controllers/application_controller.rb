df6b8cbf3ceea8cc3018576ac104ded822afa019
Kevin Doku <kjd275@gmail.com>
2025-03-10 19:22:07 -0400

    Work in progress

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::API
+  before_action: is_authorized?
+
+  def auth_headers
+    request.headers["AUTHORIZATION"]
+  end
+
+  def encode_token
+    JWT.encode(auth_headers)
+  end
+
+  def decode_token
+   if auth_headers.present?
+      # token = 
+   end
+  end
+
+  def login
+
+  end
+
+  def is_logged_in?
+
+  end
+
+  def is_authorized?
+
+  end
+  
 end
